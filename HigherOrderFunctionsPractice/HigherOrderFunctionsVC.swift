05a57e99e509b7046f6a71cee60858d8b5b4229a
Ranjeethe <Ranjeet.raushan@infoxen.com>
2020-09-02 09:53:20 +0530

    changed ViewControllername

--- HigherOrderFunctionsPractice/HigherOrderFunctionsVC.swift
+++ HigherOrderFunctionsPractice/HigherOrderFunctionsVC.swift
@@ -1,11 +1,11 @@
-//  ViewController.swift
+//  HigherOrderFunctionsVC.swift
 //  HigherOrderFunctionsPractice
 //  Created by Ranjeet Raushan on 31/08/20.
 //  Copyright © 2020 Ranjeet Raushan. All rights reserved.
 
 import UIKit
 
-class ViewController: UIViewController {
+class HigherOrderFunctionsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
